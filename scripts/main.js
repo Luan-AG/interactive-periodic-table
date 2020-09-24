574e175d7ad56efb22ffdb799d00d7979776172b
Luan Goncalves <luanarcharde@gmail.com>
2020-09-24 13:21:13 +0200

    changed the overlay from fade to slide top

--- scripts/main.js
+++ scripts/main.js
@@ -5,10 +5,12 @@ button.addEventListener("click", turnOn);
 skip.addEventListener("click", turnOff);
 
 function turnOn() {
-    document.querySelector(".overlay").style.display = "grid";
+    document.querySelector(".overlay").style.bottom = "0";
+    document.querySelector(".overlay").style.height = "100%";
 }
 
 function turnOff() {
-    document.querySelector(".overlay").style.display = "none";
+    document.querySelector(".overlay").style.bottom = "100%";
+    document.querySelector(".overlay").style.height = "0";
 }
 
